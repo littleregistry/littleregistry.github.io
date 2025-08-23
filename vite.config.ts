06013ab510d4f9a3242a061acaa44a4d781b0658
Kareem Kwong <kareem.kwong@hey.com>
2025-08-23 10:35:37 +0000

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode != 'development' ? "registry-boutique-magic" : "./"
+  base: mode != 'development' ? "/registry-boutique-magic/" : "./"
 }));
