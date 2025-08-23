7701e29c1eb7bfc5b6654075cb5198ca7b140a76
Kareem Kwong <kareem.kwong@hey.com>
2025-08-23 11:01:12 +0000

    Add basename for production

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/registry-boutique-magic">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/terms" element={<Terms />} />
